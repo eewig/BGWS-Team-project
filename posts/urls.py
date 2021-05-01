5a77e1bcf2f2c42d0a7805459f8f221a14318f2f
eewig <fovel.ti@gmail.com>
2021-05-01 16:57:41 +0300

    fixed posts

--- posts/urls.py
+++ posts/urls.py
@@ -9,9 +9,9 @@ from .views import (
 )
 
 urlpatterns = [
-	path('<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
-	path('<int:pk>/', PostDetailView.as_view(), name='post_detail'),
-	path('<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
+	path('<pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
+	path('detail/<pk>/', PostDetailView.as_view(), name='post_detail'),
+	path('<pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
 	path('new/', PostCreateView.as_view(), name='post_new'),
 	path('', PostListView.as_view(), name='post_list'),
 ]
