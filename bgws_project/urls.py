7c7a9556e169406d114ac64d6f6e0302cf4b88a8
eewig <fovel.ti@gmail.com>
2021-05-05 14:46:44 +0300

    added staticfiles

--- bgws_project/urls.py
+++ bgws_project/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('users/', include('django.contrib.auth.urls')),
     path('posts/', include('posts.urls')),
     path('', include('pages.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
